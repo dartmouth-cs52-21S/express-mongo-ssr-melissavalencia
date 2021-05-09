13e1e03a1ae60155a6e8b7d48bcd1b2b5f390fde
Melissa Valencia <melissa.valencia.23@dartmouth.edu>
2021-05-09 13:03:37 -0700

    sa6 done

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+import * as Polls from './controllers/poll_controller';
 
 // initialize
 const app = express();
@@ -28,13 +30,55 @@ app.use(express.json()); // To parse the incoming requests with JSON payloads
 // additional init stuff should go before hitting the routing
 
 // default index route
-app.get('/', (req, res) => {
-  res.send('hi');
-});
+// app.get('/', (req, res) => {
+//   res.send('hi');
+// });
+// app.get('/', (req, res) => {
+// we will later be able to get the polls by calling a function, but let's pass in no polls for now
+//   const polls = [];
+//   res.render('index', { polls });
+// });
 
 // START THE SERVER
-// =============================================================================
 const port = process.env.PORT || 9090;
 app.listen(port);
 
 console.log(`listening on: ${port}`);
+
+// DB Setup
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/cs52poll';
+
+mongoose.connect(mongoURI).then(() => {
+  console.log('connected to database:', mongoURI);
+}).catch((err) => {
+  console.log('error: could not connect to db:', err);
+});
+
+app.get('/', (req, res) => {
+  Polls.getPolls().then((polls) => {
+    res.render('index', { polls });
+  }).catch((error) => {
+    res.send(`error: ${error}`);
+  });
+});
+
+app.get('/new', (req, res) => {
+  res.render('new');
+});
+
+app.post('/new', (req, res) => {
+  const newpoll = {
+    text: req.body.text,
+    imageURL: req.body.imageURL,
+  };
+  Polls.createPoll(newpoll).then((poll) => {
+    res.redirect('/');
+  });
+});
+
+app.post('/vote/:id', (req, res) => {
+  const vote = (req.body.vote === 'up');// convert to bool
+  Polls.vote(req.params.id, vote).then((result) => {
+    res.send(result);
+  });
+});
